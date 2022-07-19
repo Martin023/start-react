93c11f0ce94840902cca62c8c306823c143ff92f
martin023 <mainamartin023@gmail.com>
2022-07-19 18:04:37 +0300

    Functional and class components

---
+++ src/components/Greet.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const Greet = () => <h2>Hello welcome to react functional compoment</h2>
+
+// function Greet(){
+//     return <h2>Hello welcome to react functional compoment</h2>
+// }
+
+// export this component to the root component so as to be recoginsed in the application
+export default Greet //this is called the default exporting way 
+
+// another way of exporting is:
+
+// export const Greet = () => <h2>Hello welcome to react functional compoment</h2>
+
+// Where you have to import it in the app component with the xaact func name ie GREET
---
+++ src/components/Welcome.js
@@ -0,0 +1,8 @@
+import React, {Component} from "react";
+ 
+class Welcome extends Component {
+    render(){
+        return <h2>Welcome to class components</h2>
+    }
+}
+export default Welcome
