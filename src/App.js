e0b1c4ac69d29db501f3ca7bdde2b4fe6cee942e
Martin023 <mainamartin023@gmail.com>
2022-08-15 23:07:47 +0300

    Destructuring class components

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ function App() {
   return (
     <div className="App">
       {/* //including greet function component */}
-      <Greet name='kelvin' hero='Batman'> <p>hello child prop </p></Greet>
+      {/* <Greet name='kelvin' hero='Batman'> <p>hello child prop </p></Greet>
       <Greet name='Brian' hero='Superman'><button>Press me</button></Greet>
-      <Greet name='Vostle' hero='Spiderman'></Greet>
+      <Greet name='Vostle' hero='Spiderman'></Greet> */}
       {/*  or <Greet/> if there is no content btwn the tags */}
 
       {/* importing a class component */}
-      {/* <Welcome name='Crazy' heroName ='Riddler'><p>Joker</p></Welcome>
-      <Message></Message> */}
+      <Welcome name='Crazy' heroName ='Riddler'><p>Joker</p></Welcome>
+      {/* <Message></Message> */}
       
       {/* <Home></Home> */}
 
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,26 @@
 import React, {Component} from "react";
  
+// class Welcome extends Component {
+//     render(){
+//         // return <h2>Welcome to class components</h2>
+//         return (
+//             <div>
+//                 <h1>Welcome to {this.props.name} i am {this.props.heroName}</h1>
+//                 {this.props.children}
+//             </div>
+//         )
+//     }
+// }
+
+// class comp desructuring - usually done within the render method.
 class Welcome extends Component {
     render(){
+
+        const {name, heroName} = this.props
         // return <h2>Welcome to class components</h2>
         return (
             <div>
-                <h1>Welcome to {this.props.name} i am {this.props.heroName}</h1>
+                <h1>Welcome to {name} i am {heroName}</h1>
                 {this.props.children}
             </div>
         )
