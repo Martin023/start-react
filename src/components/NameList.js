dea772248a3c6c88d31895887329071a6cb02365
Martin023 <mainamartin023@gmail.com>
2022-08-25 15:04:37 +0300

    Using index of elemets as key prop

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -15,19 +15,30 @@ function NameList() {
 //   )
 
 
-const persons = [
-    { id: 1, name: 'John', age: 30 },
-    { id: 2, name: 'Paul', age: 35 },
-    { id: 3, name: 'George', age: 40 }, 
-    { id: 4, name: 'Ringo', age: 45 }
 
-]
+// rendering items with no unique props
+const names = ['John', 'Paul', 'George', 'John']
+
+// The arrow function within the map method may take a 2nd parameter index to use the index as the key prop
+const nameList = names.map((name,index) => <h2 key={index}>{index} {name}</h2>  )
+return <div>{nameList}</div>
+
+
+// displaying list items with unique properties
+// const persons = [
+//     { id: 1, name: 'John', age: 30 },
+//     { id: 2, name: 'Paul', age: 35 },
+//     { id: 3, name: 'George', age: 40 }, 
+//     { id: 4, name: 'Ringo', age: 45 }
+
+// ]
+
 // list items are recommended to have key properties ie key = {person.id} to avoid duplicate items and a console error in browser
 // key property is a special attribute that you need to include when creating lists of elements
 
  
-const personList = persons.map((person) => <Person key={person.id} person={person} ></Person>)
-return (<div>{personList}</div>)
+// const personList = persons.map((person) => <Person key={person.id} person={person} ></Person>)
+// return (<div>{personList}</div>)
 
 }
 
