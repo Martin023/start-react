485c41a670e9ae94be05edc2109ca5e2a9512792
Martin023 <mainamartin023@gmail.com>
2022-08-25 14:38:41 +0300

    Adding and imporntance of key property in rendering list items

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -18,12 +18,15 @@ function NameList() {
 const persons = [
     { id: 1, name: 'John', age: 30 },
     { id: 2, name: 'Paul', age: 35 },
-    { id: 3, name: 'George', age: 40 },
+    { id: 3, name: 'George', age: 40 }, 
     { id: 4, name: 'Ringo', age: 45 }
 
 ]
+// list items are recommended to have key properties ie key = {person.id} to avoid duplicate items and a console error in browser
+// key property is a special attribute that you need to include when creating lists of elements
 
-const personList = persons.map((person) => <Person person={person} ></Person>)
+ 
+const personList = persons.map((person) => <Person key={person.id} person={person} ></Person>)
 return (<div>{personList}</div>)
 
 }
