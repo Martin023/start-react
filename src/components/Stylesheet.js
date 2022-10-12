96d98c2a4b7d2e98fc78bf87f2ef292b811c2dac
Martin023 <mainamartin023@gmail.com>
2022-10-13 02:44:35 +0300

    # ways of styling a component using css

--- src/components/Stylesheet.js
+++ src/components/Stylesheet.js
@@ -1,13 +1,28 @@
 import React from 'react'
+import InlineStyling from './InlineStyling'
 import './mystyles.css'
 
 
 function Stylesheet() {
     let className = 'primary'
+    // To include inline InlineStyling, we need to use the style attribute and pass an 
+    // object with the style properties written in camelCase.
+    // We then add the style object to the JSX element.
+    styles = {
+      fontSize: '72px'
+    }
   return (
     <div>
-        {/* or directly put the attribute name */}
+        {/* or directly put the attribute name  */}
+        {/* This is for external stylesheet */}
         <h1 className={`${className} fontsx blueback`}>Stylesheet</h1>
+
+        {/* This is for inline styling ie css in same file styling */}
+
+        <h1 style={styles}>Inline</h1>
+
+        {/* for direct inline styling of the elements , use double braces without the style object*/}
+        <h1 style={{fontSize:20 }}></h1>
 
         <h4 >clasnames use</h4>
     </div>
